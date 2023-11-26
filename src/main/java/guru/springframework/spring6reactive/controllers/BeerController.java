6f6e34a0b378cd3cfacff9966efd89ed88db26f2
Rafał Zubala <rzubala@gmail.com>
2023-11-26 12:13:01 +0100

    controller

---
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -0,0 +1,17 @@
+package guru.springframework.spring6reactive.controllers;
+
+import guru.springframework.spring6reactive.model.BeerDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class BeerController {
+
+    public static final String BEER_PATH = "/api/v2/beer";
+
+    @GetMapping(BEER_PATH)
+    public Flux<BeerDTO> listBeers() {
+        return Flux.just(BeerDTO.builder().id(1).build(), BeerDTO.builder().id(2).build());
+    }
+}
